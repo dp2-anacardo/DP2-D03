cbdfc4da5acde9847c13cddd1bf329b37d5050f2
miguelvr98 <miguelvr98@gmail.com>
2019-04-19 19:05:41 +0200

    Solo faltan los coverages de los test

--- src/main/java/domain/Application.java
+++ src/main/java/domain/Application.java
@@ -3,12 +3,7 @@ package domain;
 
 import java.util.Date;
 
-import javax.persistence.Access;
-import javax.persistence.AccessType;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -21,6 +16,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = {
+		@Index(columnList = "status")
+})
 public class Application extends DomainEntity {
 
 	private Date		moment;
