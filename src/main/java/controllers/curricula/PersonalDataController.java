b1d8ed1d1eac626367bc3394136db96acd7bd398
Servesus <jesusgarrocho1998@gmail.com>
2019-04-23 01:48:47 +0200

    Controllers arreglados

--- src/main/java/controllers/curricula/PersonalDataController.java
+++ src/main/java/controllers/curricula/PersonalDataController.java
@@ -1,13 +1,10 @@
+
 package controllers.curricula;
 
-import controllers.AbstractController;
-import domain.Actor;
-import domain.Curricula;
-import domain.Hacker;
-import domain.PersonalData;
+import javax.validation.ValidationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.util.Assert;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,70 +12,72 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
 import services.ActorService;
 import services.HackerService;
 import services.PersonalDataService;
-
-import javax.naming.Binding;
-import javax.validation.ValidationException;
+import controllers.AbstractController;
+import domain.Actor;
+import domain.Curricula;
+import domain.Hacker;
+import domain.PersonalData;
 
 @Controller
 @RequestMapping("/personalData/hacker")
 public class PersonalDataController extends AbstractController {
 
-    @Autowired
-    private PersonalDataService personalDataService;
+	@Autowired
+	private PersonalDataService	personalDataService;
 
-    @Autowired
-    private ActorService actorService;
+	@Autowired
+	private ActorService		actorService;
 
-    @Autowired
-    private HackerService hackerService;
-
-    @RequestMapping(value = "/edit",method = RequestMethod.GET)
-    public ModelAndView edit(@RequestParam int personalDataId){
-        ModelAndView result;
-        try{
-            PersonalData p = this.personalDataService.findOne(personalDataId);
-            Actor a = this.actorService.getActorLogged();
-            Hacker h = this.hackerService.findOne(a.getId());
-            boolean canEdit = false;
-            for(Curricula c: h.getCurricula()){
-                if(c.getPersonalData().equals(p))
-                    canEdit = true;
-            }
-            Assert.isTrue(canEdit);
-            result = this.createEditModelAndView(p,null);
-        }catch(Throwable oops){
-            result = new ModelAndView("redirect:/misc/403");
-        }
-        return result;
-    }
-
-    @RequestMapping(value = "/edit", method = RequestMethod.POST)
-    public ModelAndView save(@ModelAttribute("personalD") PersonalData p, BindingResult binding){
-        ModelAndView result;
-        try{
-            p = this.personalDataService.reconstruct(p,binding);
-            p = this.personalDataService.save(p);
-            result = new ModelAndView("redirect:/curricula/hacker/list.do");
-        }catch(final ValidationException e){
-            result = this.createEditModelAndView(p,null);
-        }catch(Throwable oops){
-            result = this.createEditModelAndView(p,"curricula.commit.error");
-        }
-        return result;
-    }
+	@Autowired
+	private HackerService		hackerService;
 
 
-    private ModelAndView createEditModelAndView(PersonalData p, String messageCode){
-        ModelAndView result;
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam final int personalDataId) {
+		ModelAndView result;
+		try {
+			final PersonalData p = this.personalDataService.findOne(personalDataId);
+			final Actor a = this.actorService.getActorLogged();
+			final Hacker h = this.hackerService.findOne(a.getId());
+			boolean canEdit = false;
+			for (final Curricula c : h.getCurricula())
+				if (c.getPersonalData().equals(p))
+					canEdit = true;
+			Assert.isTrue(canEdit);
+			result = this.createEditModelAndView(p, null);
+		} catch (final Throwable oops) {
+			result = new ModelAndView("redirect:/");
+		}
+		return result;
+	}
 
-        result = new ModelAndView("personalData/hacker/edit");
-        result.addObject("personalD", p);
-        result.addObject("message", messageCode);
+	@RequestMapping(value = "/edit", method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute("personalD") PersonalData p, final BindingResult binding) {
+		ModelAndView result;
+		try {
+			p = this.personalDataService.reconstruct(p, binding);
+			p = this.personalDataService.save(p);
+			result = new ModelAndView("redirect:/curricula/hacker/list.do");
+		} catch (final ValidationException e) {
+			result = this.createEditModelAndView(p, null);
+		} catch (final Throwable oops) {
+			result = this.createEditModelAndView(p, "curricula.commit.error");
+		}
+		return result;
+	}
 
-        return result;
+	private ModelAndView createEditModelAndView(final PersonalData p, final String messageCode) {
+		ModelAndView result;
 
-    }
+		result = new ModelAndView("personalData/hacker/edit");
+		result.addObject("personalD", p);
+		result.addObject("message", messageCode);
+
+		return result;
+
+	}
 }
--- src/main/java/controllers/curricula/PositionDataController.java
+++ src/main/java/controllers/curricula/PositionDataController.java
@@ -1,8 +1,8 @@
+
 package controllers.curricula;
 
-import controllers.AbstractController;
-import domain.EducationalData;
-import domain.PositionData;
+import javax.validation.ValidationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -11,85 +11,87 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
 import services.ActorService;
 import services.HackerService;
 import services.PositionDataService;
-
-import javax.validation.ValidationException;
+import controllers.AbstractController;
+import domain.PositionData;
 
 @Controller
 @RequestMapping("/positionData/hacker")
 public class PositionDataController extends AbstractController {
-    @Autowired
-    private PositionDataService positionDataService;
-    @Autowired
-    private ActorService actorService;
-    @Autowired
-    private HackerService hackerService;
+
+	@Autowired
+	private PositionDataService	positionDataService;
+	@Autowired
+	private ActorService		actorService;
+	@Autowired
+	private HackerService		hackerService;
 
 
-    @RequestMapping(value = "/create", method = RequestMethod.GET)
-    public ModelAndView create(@RequestParam int curriculaId){
-        ModelAndView result;
-        try{
-            PositionData p = this.positionDataService.create();
-            result = this.createEditModelAndView(p,null,curriculaId);
-        }catch(Throwable oops){
-            result = new ModelAndView("redirect:/misc/403");
-        }
-        return result;
-    }
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create(@RequestParam final int curriculaId) {
+		ModelAndView result;
+		try {
+			final PositionData p = this.positionDataService.create();
+			result = this.createEditModelAndView(p, null, curriculaId);
+		} catch (final Throwable oops) {
+			result = new ModelAndView("redirect:/");
+		}
+		return result;
+	}
 
-    @RequestMapping(value = "/edit", method = RequestMethod.GET)
-    public ModelAndView edit(@RequestParam int curriculaId, @RequestParam int positionDataId){
-        ModelAndView result;
-        try{
-            PositionData e = this.positionDataService.findOne(positionDataId);
-            result = this.createEditModelAndView(e,null,curriculaId);
-        }catch(Throwable oops){
-            result = new ModelAndView("redirect:/misc/403");
-        }
-        return result;
-    }
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam final int curriculaId, @RequestParam final int positionDataId) {
+		ModelAndView result;
+		try {
+			final PositionData e = this.positionDataService.findOne(positionDataId);
+			result = this.createEditModelAndView(e, null, curriculaId);
+		} catch (final Throwable oops) {
+			result = new ModelAndView("redirect:/");
+		}
+		return result;
+	}
 
-    @RequestMapping(value = "/edit", method = RequestMethod.POST)
-    public ModelAndView save(@ModelAttribute("positionD") PositionData positionData, @RequestParam int curriculaId, BindingResult binding){
-        ModelAndView result;
-        try{
-            positionData = this.positionDataService.reconstruct(positionData,binding);
-            positionData = this.positionDataService.save(positionData,curriculaId);
-            result = new ModelAndView("redirect:/curricula/hacker/list.do");
-        }catch(final ValidationException e){
-            result = this.createEditModelAndView(positionData,null,curriculaId);
-        }catch(Throwable oops){
-            result = this.createEditModelAndView(positionData,"curricula.commit.error",curriculaId);
-        }
-        return result;
-    }
+	@RequestMapping(value = "/edit", method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute("positionD") PositionData positionData, @RequestParam final int curriculaId, final BindingResult binding) {
+		ModelAndView result;
+		try {
+			positionData = this.positionDataService.reconstruct(positionData, binding);
+			positionData = this.positionDataService.save(positionData, curriculaId);
+			result = new ModelAndView("redirect:/curricula/hacker/list.do");
+		} catch (final ValidationException e) {
+			result = this.createEditModelAndView(positionData, null, curriculaId);
+		} catch (final Throwable oops) {
+			result = this.createEditModelAndView(positionData, "curricula.commit.error", curriculaId);
+		}
+		return result;
+	}
 
-    @RequestMapping(value = "/delete",method = RequestMethod.GET)
-    public ModelAndView delete(@RequestParam int positionDataId){
-        ModelAndView result;
-        try{
-            PositionData e = this.positionDataService.findOne(positionDataId);
-            this.positionDataService.delete(e);
-            result = new ModelAndView("redirect:/curricula/hacker/list.do");
-        }catch(Throwable oops){
-            result = new ModelAndView("redirect:/misc/403");
-        }
-        return result;
-    }
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	public ModelAndView delete(@RequestParam final int positionDataId) {
+		ModelAndView result;
+		try {
+			final PositionData e = this.positionDataService.findOne(positionDataId);
+			this.positionDataService.delete(e);
+			result = new ModelAndView("redirect:/curricula/hacker/list.do");
+		} catch (final Throwable oops) {
+			result = new ModelAndView("redirect:/");
+		}
+		return result;
+	}
 
-    private ModelAndView createEditModelAndView(PositionData p, String messageCode,int curriculaId){
-        ModelAndView result;
+	private ModelAndView createEditModelAndView(final PositionData p, final String messageCode, final int curriculaId) {
+		ModelAndView result;
 
-        result = new ModelAndView("positionData/hacker/edit");
-        result.addObject("positionD", p);
-        result.addObject("message", messageCode);
-        result.addObject("curriculaId",curriculaId);
+		result = new ModelAndView("positionData/hacker/edit");
+		result.addObject("positionD", p);
+		result.addObject("message", messageCode);
+		result.addObject("curriculaId", curriculaId);
 
-        return result;
+		return result;
 
-    }
+	}
 
 }
