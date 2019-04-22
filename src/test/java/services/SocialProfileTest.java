4c1a6fdaa2a554f4ef4ebeb3d42e3a2ecefb4122
MrGaRRos <juanmagarrocho@gmail.com>
2019-04-23 00:37:15 +0200

    Test social profile, falta coverages

--- src/test/java/services/SocialProfileTest.java
+++ src/test/java/services/SocialProfileTest.java
@@ -23,6 +23,13 @@ public class SocialProfileTest extends AbstractTest {
     private SocialProfileService socialProfileService;
 
 
+    /*
+     * Testing functional requirement : 23.1 An actor that is authenticated must be able to manage their social profiles
+     * Positive: A admin create a social profile
+     * Negative: A admin tries to create a social profile with invalid data
+     * Sentence coverage: 82%
+     * Data coverage: 40%
+     */
     @Test
     public void createSocialProfileDriver(){
         Object testingData [][] ={
@@ -58,28 +65,34 @@ public class SocialProfileTest extends AbstractTest {
 
         super.checkExceptions(expected,caught);
     }
-/*
 
+    /*
+     * Testing functional requirement : 23.1 An actor that is authenticated must be able to manage their social profiles
+     * Positive: A admin edit a social profile
+     * Negative: A admin tries to edit a social profile of other admin
+     * Sentence coverage: 82%
+     * Data coverage: 40%
+     */
     @Test
     public void editSocialProfileDriver(){
         Object testingData[][] = {
                 {
-                    "pruebesita","admin1", null
+                        "socialProfileAdmin1", "pruebesita","admin1", null
                 }, {
-
-                }
+                    "socialProfileAdmin1", "pruebesita","admin2", IllegalArgumentException.class
+        }
         };
         for (int i = 0; i<testingData.length; i++)
-            this.editSocialProfileTemplate((String) testingData[i][0],(String) testingData[i][1], (Class<?>) testingData[i][2]);
+            this.editSocialProfileTemplate(super.getEntityId((String) testingData[i][0]),(String) testingData[i][1],(String) testingData[i][2], (Class<?>) testingData[i][3]);
     }
 
-    private void editSocialProfileTemplate(String s1, String admin, Class<?> expected){
+    private void editSocialProfileTemplate(int entityId, String s1, String admin, Class<?> expected){
         Class<?> caught;
         caught = null;
 
         try{
             this.authenticate(admin);
-            SocialProfile s = this.socialProfileService.findOne();
+            SocialProfile s = this.socialProfileService.findOne(entityId);
             s.setNick(s1);
             DataBinder binding = new DataBinder(new SocialProfile());
             s = this.socialProfileService.reconstruct(s, binding.getBindingResult());
@@ -88,5 +101,38 @@ public class SocialProfileTest extends AbstractTest {
             caught = oops.getClass();
         }
         super.checkExceptions(expected, caught);
-    } */
+    }
+
+    /*
+     * Testing functional requirement : 23.1 An actor that is authenticated must be able to manage their social profiles
+     * Positive: A admin delete a social profile
+     * Negative: A admin tries to delete a social profile of other admin
+     * Sentence coverage: 82%
+     * Data coverage: 40%
+     */
+    @Test
+    public void deleteSocialProfileDriver(){
+        Object testingData[][] = {
+                {
+                        "socialProfileAdmin1","admin2", IllegalArgumentException.class
+                }, {
+                        "socialProfileAdmin1","admin1", null
+                }
+        };
+        for (int i = 0; i<testingData.length; i++)
+            this.deleteSocialProfileTemplate(super.getEntityId((String) testingData[i][0]),(String) testingData[i][1], (Class<?>) testingData[i][2]);
+    }
+
+    private void deleteSocialProfileTemplate(int entityId, String admin, Class<?> expected){
+        Class<?> caught;
+        caught = null;
+        try{
+            this.authenticate(admin);
+            SocialProfile s = this.socialProfileService.findOne(entityId);
+            this.socialProfileService.delete(s);
+        } catch (Throwable oops){
+            caught = oops.getClass();
+        }
+        super.checkExceptions(expected, caught);
+    }
 }
